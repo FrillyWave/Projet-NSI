1c256129ad9bb6edd72bc736eb711440a342ebf3
FrillyWave <jojo.gour07@gmail.com>
2025-04-07 11:59:54 +0200

    Update gameServer.js

--- gameServer.js
+++ gameServer.js
@@ -1,1 +1,31 @@
-const express = required('express')
+const express = require('express')
+const { spawn } = require('child_process')
+const path = require('path')
+const app = express()
+
+const port = 4000 
+
+const scriptPath = path.join(__dirname, "Backend", "projet_Python.py")
+
+app.get("/api/run-python", (req, res) => {
+    const pythonProcess = spawn("python3", [scriptPath]);
+
+    let output = "";
+    pythonProcess.stdout.on("data", (data) => {
+        output += data.toString();
+    });
+
+    pythonProcess.on("close", (code) => {
+        console.log(`Processus Python terminé avec le code ${code}`);
+        res.json({ output });
+    });
+    
+    pythonProcess.stderr.on("data", (data) => {
+        console.error(`Erreur Python : ${data}`);
+    });
+    
+});
+
+app.listen(port, () => {
+    console.log(`Jeu serveur Express en écoute sur http://localhost:${port}`);
+});
