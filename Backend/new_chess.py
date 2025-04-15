12a52a55fa1031b64eea04f4ae1b17efb60332e7
KeeptMantella <Leleuju@gmail.com>
2025-04-15 14:07:04 +0200

    Update new_chess.py

--- Backend/new_chess.py
+++ Backend/new_chess.py
@@ -1,6 +1,14 @@
 import chess
+import requests 
+import json 
 
-board = chess.Board()
-print(board)
-print(board.fen())
-
+def Main():
+    url = "https://projet-nsi-sffl.onrender.com"
+    board = chess.Board()
+    requests.post(url, json = board.fen())
+    while not(board.is_checkmate()) and not(board.is_stalemate()) and not(board.is_insufficient_material()):
+        reponse = requests.get(url)
+        board = chess.Board([0])
+        board.push(reponse[1])
+        requests.post(url, json = board.fen())
+        
